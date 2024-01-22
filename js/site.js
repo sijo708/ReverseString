5a42f8dc74a72a2d10860add755996f6edc5ec9f
sijo708 <sijo708@yahoo.com>
2024-01-22 15:28:22 -0500

    init commit

--- js/site.js
+++ js/site.js
@@ -1,12 +1,35 @@
-function displayMessage(){
-    let inputBox = document.getElementById('message');
-    let message = inputBox.value;
+function getValues() {
+    let userMessage = document.getElementById('message').value;
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: message
-        }
-    );
+    let reversed = reverseString(userMessage)
+
+    displayMessage(reversed);
+
+}
+
+function reverseString(inputString){
+    // turn thestring to array
+    let characters = inputString.split('');
+    let result = [];
+
+    // loop through the array from te end towards the beginning
+    for(let index = characters.length - 1; index >= 0; index = index - 1){
+        let letter = characters[index];
+        // put each letter into a new array
+        result.push(letter);
+    }
+
+    // trun the new array back into a string
+    let resultAsString = result.join('');
+
+    // return the string
+    return resultAsString;
+}
+
+function displayMessage(reversedString){
+    // put our message in the HTML
+    document.getElementById('result').innerHTML = `Your message reversed is: ${reversedString}`;
+
+    document.getElementById('alert').classList.remove('invisible');
+
 }
